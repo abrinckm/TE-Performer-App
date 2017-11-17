e6bd5a5dfc646cbd64ec488ccd150071fa2e9f38
Adam Brinckman <abrinckm@gmail.com>
2017-11-17 08:14:08 -0500

    Added lodash, request-promise, and a base wrapper GET request

--- src/wrapper/base-wrapper.js
+++ src/wrapper/base-wrapper.js
@@ -1,4 +1,6 @@
 const config = require('config');
+const request = require('request-promise');
+const _ = require('lodash');
 
 class BaseWrapper {
   constructor(params) {
@@ -10,6 +12,20 @@ class BaseWrapper {
     }
     this.apiUrl = config.get('apiUrl');
   }   
+
+  // --------------------
+  // A simple GET request
+  get(path, options) {
+    options = _.merge({
+      headers: {
+        'content-type': 'application/json'
+      }
+    }, options || {});
+
+    options.uri = `${this.apiUrl}${path}`;
+
+    return request(options);
+  }
 }
 
 module.exports = BaseWrapper;
